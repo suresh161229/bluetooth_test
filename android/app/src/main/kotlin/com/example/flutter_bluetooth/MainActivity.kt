37b5c5f1ce94ccfd384c4bde2d0a9e43b41750fc
suresh161229 <70518214+suresh161229@users.noreply.github.com>
2024-11-03 23:32:28 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/flutter_bluetooth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_bluetooth/MainActivity.kt
@@ -2,5 +2,32 @@ package com.example.flutter_bluetooth
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "flutter_bluetooth"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "isBluetoothAvailable" -> {
+                    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+                    if (bluetoothAdapter != null) {
+                        result.success(true)
+                    } else {
+                        result.success(false)
+                    }
+                }
+                "isBluetoothEnabled" -> {
+                    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+                    if (bluetoothAdapter != null && bluetoothAdapter.isEnabled) {
+                        result.success(true)
+                    } else {
+                        result.success(false)
+                    }
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
 }
+
